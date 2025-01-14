255b7000826e93e311a2c1e20e427953e395a26f
MateusRsz <matheusestudante69@gmail.com>
2025-01-14 15:39:33 -0300

    f

--- exercicios/aula05/index2.js
+++ exercicios/aula05/index2.js
@@ -1,13 +1,79 @@
-const soma = (...valores) => {
-
-    const somar = val =>{
-        let res=0
-        for (v of val){
-            res+=v
-        }
-        return res
-    }
-    
-    return somar(valores)
+function* perguntas(){
+    const nome= yield "qual seu nome?"
+    const esporte= yield"qual seu esporte favorito?"
+    return `Olá ${nome}, seu esporte favortio é ${esporte}`
 }
-console.log(soma(2,23,32,3,33))
+
+const itp = perguntas()
+console.log(itp.next().value)
+console.log(itp.next("Mateus").value)
+console.log(itp.next("futebol").value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function* cores(){
+//     yield 'vermelho'
+//     yield 'verde'
+//     yield 'azul'
+//     yield "acabou meu filho"
+//     yield 'ja disse que ACABOU'
+//     yield 'chatão vc é'
+// }
+
+// let itc = cores()
+// console.log(itc.next().value)
+// console.log(itc.next().value)
+// console.log(itc.next().value)
+// console.log(itc.next().value)
+// console.log(itc.next().value)
+// console.log(itc.next().value) 
+// console.log(itc.next().value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const soma = (...valores) => {
+
+//     const somar = val =>{
+//         let res=0
+//         for (v of val){
+//             res+=v
+//         }
+//         return res
+//     }
+    
+//     return somar(valores)
+// }
+// console.log(soma(2,23,32,3,33))
