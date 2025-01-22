ee7e8314fed8039b7d8da4b269d51e9e53236fa8
MateusRsz <matheusestudante69@gmail.com>
2025-01-22 13:43:00 -0300

    function map

--- exercicios/aula06/index.js
+++ exercicios/aula06/index.js
@@ -1,6 +1,25 @@
-const cursos = ['javascript', 'css', 'python', 'react']
-let c = cursos.map((elementes, indice)=>{
-    return `<div>${elementes}</div>`
-}) 
+const convertInt = (e)=>parseInt(e)
+const dobrar = (e)=>e*2
+let num = [2.4, 53.3, 4.5,9.2,34.4,423,32].map(convertInt).map(dobrar)
+for (v of num){
+console.log(v)
+}
 
-console.log(c)
+
+// const el = document.getElementsByTagName('div')
+// const val = Array.prototype.map.call(el, (valor) => valor)
+// console.log(val)
+
+
+// let el = [...document.getElementsByTagName('div')]
+// el.map( (el, i) =>{
+//     el.style.color = 'blue'
+// })
+// console.log(el)
+
+
+// const cursos = ['javascript', 'css', 'python', 'react']
+// let c = cursos.map((elementes, indice)=>{
+//     return `<div>${elementes}</div>`
+// }) 
+// console.log(c)
