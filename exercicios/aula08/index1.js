f3be3f87cb0e1126e73f472a11b65f48895b2c8e
MateusRsz <matheusestudante69@gmail.com>
2025-01-31 13:47:35 -0300

    projeto simples de trocador de cor

--- exercicios/aula08/index1.js
+++ exercicios/aula08/index1.js
@@ -1,20 +1,25 @@
 let div = [...document.getElementsByTagName('div')]
 let cursos = []
-console.log(cursos)
-let btn = document.getElementById('btn').addEventListener('click', (e) => {
-})
+
 div.map((Element) => {
     Element.addEventListener('mouseenter', (e) => {
         e.target.style.backgroundColor = 'darkred'
-        Element.addEventListener('mouseout', () => {
+        Element.addEventListener('mouseout', (e) => {
             e.target.style.backgroundColor = '#cfcfcf'
         })
     })
+
     Element.addEventListener('click', (e) => {
-        cursos[e.target]
-        e.target.style.backgroundColor = 'red'
-
-
+        
+        cursos.forEach(element => {
+            if (element == element){
+                alert('n')
+            }
+             cursos.push(e.target.innerHTML)   
+            })
+        });
+ 
     })
-
-})
+let btn = document.getElementById('btn').addEventListener('click', (e) => {
+})       
+console.log(cursos)
